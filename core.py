d6b660e6591409dd256bfba0d565015311865346
fleetcaptain <fleetcaptain>
2019-06-21 14:43:10 -0700

    Changed output to write to SQLite database instead of JSON

--- core.py
+++ core.py
@@ -6,7 +6,7 @@
 # TODO - set the individual functions to return their findings via dicts, not just printString(in the function
 # unknown if other decompilers make the same output structure
 
-import sys, os, json, re
+import sys, os, json, re, sqlite3, time
 from subprocess import check_call,CalledProcessError      
 from xml.dom import minidom
 from os import listdir
@@ -24,6 +24,42 @@ debug = False
 blacklist_receivers = []
 structured_output = {} # the JSON output
 silent = False
+packagename = None
+
+
+
+
+## Database related methods
+def initDB(db):
+	db.execute('''CREATE TABLE activities (app text, activity text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE receivers (app text, receiver text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE receiver_actions (app text, receiver text, action text)''')
+	db.execute('''CREATE TABLE runtime_receivers (app text, receiver text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE services (app text, service text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE webviews (app text, webview text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE broadcaster (app text, broadcaster text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE jsbridges (app text, item text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE permissions (app text, permission text, tag text, first_seen text)''')
+	db.execute('''CREATE TABLE appinfo (app text, sdk integer, backup text, debug text)''')
+	db.commit()
+
+
+# save DB item
+def saveItem(db, table, app, item, tag, first_seen):
+	db.execute("INSERT INTO " + table + " VALUES ('" + app + "','" + item + "','" + tag + "','" + first_seen + "')")
+	db.commit()
+
+
+# save general app info
+def saveAppInfo(db, app, sdk, backup, debug):
+	db.execute("INSERT INTO appinfo VALUES ('" + app + "'," + str(sdk) + ",'" + backup + "','" + debug + "')")
+	db.commit()
+
+
+# save recevier actions
+def saveAction(db, table, app, receiver, action):
+	db.execute("INSERT INTO " + table + " VALUES ('" + app + "','" + receiver + "','" + action + "')")
+	db.commit()
 
 
 
@@ -45,7 +81,7 @@ def getAllFiles(path):
 
 
 # parse xml document looking for exported or available content providers
-def getProviders(xmldocument):
+def getProviders(xmldocument, db):
 	items = xmldocument.getElementsByTagName("provider")
 	# for each provider
 	for item in items:
@@ -66,13 +102,11 @@ def getProviders(xmldocument):
 		# final verdict for this provider
 		if (is_exported):
 			printString("[Provider] " +  str(item.attributes['android:name'].value))
-			tempd = {}
-			tempd['id'] = item.attributes['android:name'].value
-			structured_output['providers'].append(tempd)
+			saveItem(db, 'providers', packagename, item.attributes['android:name'].value, "exported", current_time)
 			
 
 # parse xml document looking for exported or available services
-def getServices(xmldocument):
+def getServices(xmldocument, db):
 	items = xmldocument.getElementsByTagName("service")
 	# for each service
 	for item in items:
@@ -93,14 +127,12 @@ def getServices(xmldocument):
 		# final verdict for this service
 		if (is_exported):
 			printString("[Service] " + str(item.attributes['android:name'].value))
-			tempd = {}
-			tempd['id'] = item.attributes['android:name'].value
-			structured_output['services'].append(tempd)
+			saveItem(db, 'services', packagename, item.attributes['android:name'].value, "exported", current_time)
 			
 
 
 # parse xml document looking for exported or available broadcast receivers
-def getReceivers(xmldocument):
+def getReceivers(xmldocument, db):
 	items = xmldocument.getElementsByTagName("receiver")
 	# for each receiver
 	for item in items:
@@ -143,21 +175,15 @@ def getReceivers(xmldocument):
 		if (is_exported):
 			printString("[Receiver] " + str(item.attributes['android:name'].value))
 			printString("\tPermission: " + permission)
-			tempd = {}
-			tempd['id'] = item.attributes['android:name'].value
-			tempd['permission'] = permission
-			tempd['actions'] = []
+			saveItem(db, 'receivers', packagename, str(item.attributes['android:name'].value), "intent-filter", current_time)
 			for action in action_list:
-				printString('\tAction: ' + action)
-				tempd['actions'].append(action)
-			printString('')
-			structured_output['receivers'].append(tempd)
+					saveAction(db, 'receiver_actions', packagename, str(item.attributes['android:name'].value), action)
 		else:
 			blacklist_receivers.append(str(item.attributes['android:name'].value))
 
 
 # parse xml document looking for exported or available activities
-def getActivities(xmldocument):
+def getActivities(xmldocument, db):
 	items = xmldocument.getElementsByTagName("activity")
 	# for each activity
 	for item in items:
@@ -179,7 +205,7 @@ def getActivities(xmldocument):
 			if (intent_filters != []):
 				is_exported = True
 				for intent_filter in intent_filters:
-					tag = "(intent-filter)"
+					tag = "intent-filter"
 					# exported due to intent filter (which may limit the exposed attack paths
 					# Now check if it's a browsable activity. These can be reached via links like on webpages,	
 					# so vulnerabilities might be easier to exploit
@@ -187,35 +213,16 @@ def getActivities(xmldocument):
 					if (categories != []):
 						for category in categories:
 							if (str(category.attributes['android:name'].value) == "android.intent.category.BROWSABLE"):
-								tag = "(browsable)"
-					'''
-					elif (search_tag == "receiver"):
-						permission = ""
-						try:
-							permission = item.attributes['android:permission'].value
-						except:
-							permission = " no permission?"
-						#printString(item.attributes['android:name'].value + " " + permission
-						# lets get some metadata about the receiver
-						if (permission != " no permission?"):
-							tag = "permission: " + item.attributes['android:permission'].value
-						actions = intent_filter.getElementsByTagName('action')
-						if (actions != []):
-							for action in actions:
-								tag = tag + "\n\t" + (str(action.attributes['android:name'].value)) + " "
-					'''
+								tag = "browsable"
 			else:
 				if (export_val == "true"): # if there are no intent filters but the activity was exported explicitly, we do want to report it
 					is_exported = True
-					tag = "(explicit)"
+					tag = "exported"
 				
 		# final verdict for this activity
 		if (is_exported):
 			printString("[Activity] " + item.attributes['android:name'].value + " " + str(is_exported) + " " + tag)
-			tempd = {}
-			tempd['id'] = item.attributes['android:name'].value
-			tempd['method'] = tag
-			structured_output['activities'].append(tempd)
+			saveItem(db, 'activities', packagename, item.attributes['android:name'].value, tag, current_time)
 
 
 
@@ -233,7 +240,6 @@ parser.add_option("-j", "--java", dest="java", help="directory with the app's de
 parser.add_option("--high-confidence", action="store_true", dest="confidence", help="do not return low quality hits in source code search (ex: skip broadcasters in files with LocalBroadcastManager imported")
 parser.add_option("-o", "--output", dest="out_file", help="Write results to specified output file")
 parser.add_option("--debug", dest="debug", action="store_true", help="Enable verbose debug output")
-parser.add_option("--json", dest="json", action="store_true", help="output results in JSON format for parsing by other scripts")
 parser.add_option("--secrets", dest="scan_secrets", help="JSON regex file containing search terms for API keys, tokens, and other sensitive data")
 
 (options, args) = parser.parse_args()
@@ -243,31 +249,16 @@ scan_secrets = options.scan_secrets
 java = options.java
 confidence = options.confidence
 manifest = options.manifest
-usejson = options.json
+
+current_time = str(time.time())
 
 check_all = True
-#printString('Usage: core.py [directory with decompiled app folders] [debug true or false]'
 
 # input - path to AndroidManifest.xml
 if (manifest == None):
 	printString('You must specify an input manifest file')
 	printString(parser.usage)
 	exit()
-
-# analyzing multiple APK folders at once is confusing
-# Stick with the *nix principal of one tool, one purpose
-# if users wish to analyze multiple folders, they can wrap this tool in a script
-# at least, for now...
-'''
-for name in os.listdir(app_folder_directory):
-            if os.path.isdir(os.path.join(app_folder_directory, name)):
-		app_folders.append(app_folder_directory + name)
-		if (debug):
-			printString("Added app " + name + " to analysis list"
-'''
-
-if (usejson):
-	silent = True # no output anything but json
 
 if (scan_secrets != None):
 	# read in regex rules
@@ -282,31 +273,26 @@ if (scan_secrets != None):
 if (confidence):
 	check_all = False
 
-# XML parsing of AndroidManifest.xml
-# check:
-# - backup (allowed, disallowed, not specified)
-# - exported activities
-# - activites with intent-filters
-# - exported providers
-# - exported services
+conn = sqlite3.connect('apps.db')
+initDB(conn)
 
+# parse manifest
 xmldoc = minidom.parse(manifest)
 packagename = xmldoc.getElementsByTagName('manifest')[0].attributes['package'].value
-structured_output['app'] = packagename
 
 printString('\n-------------------------------------')
 printString('-- Results for ' + packagename)
 printString('')#printString('##############################')
 
 printString('-- Misc Info --')
-# target SDK
+
 uses_sdk = xmldoc.getElementsByTagName('uses-sdk')
 target_api = 0
 try:
 	target_api = int(uses_sdk[0].attributes['android:targetSdkVersion'].value)
 except:
-	target_api = 'not specified'
-structured_output['target_api'] = target_api
+	target_api = '-1'
+
 printString("Target API: " + str(target_api))
 if (target_api < 17):
 	printString("Target API is old, check manifest manually for further vulnerabilities")
@@ -318,8 +304,7 @@ try:
 except Exception as e:
 	# not set
 	#printString(e
-	backupvalue = "True (implicit)"
-structured_output['backup'] = backupvalue
+	backupvalue = "true"
 printString("Backup set: " + backupvalue)
 
 # debuggable
@@ -327,9 +312,11 @@ try:
 	debuggable = str(application[0].attributes['debuggable'].value)
 except Exception as e:
 	# not set
-	debuggable = "False (implicit)"
-structured_output['debuggable'] = debuggable
+	debuggable = "false"
 printString("Debuggable: " + debuggable)
+
+
+saveAppInfo(conn, packagename, target_api, backupvalue, debuggable)
 
 # permissions
 permissions = xmldoc.getElementsByTagName('uses-permission')
@@ -338,56 +325,31 @@ for permission in permissions:
 	p_name = str(permission.attributes['android:name'].value)
 	if p_name in flagged_permissions:
 		printString("Uses: " + p_name)
-		structured_output["permissions"].append(p_name)
+		saveItem(conn, "permissions", packagename, p_name, "", current_time)
 
-# native libraries
-'''
-native_dirs = []
-try:
-	native_dirs = os.listdir(path + 'lib/')
-except:
-	# does not use native dirs?
-	if (debug):
-		printString("[debug] no native libraries"
-if (debug):
-	printString('[debug] Subdirs of lib/: ' + str(native_dirs)
-if (len(native_dirs) > 0):
-	printString('Uses native code'
-'''
 
 # All exported components that the manifest reveals
-# Note that some broadcast receivers and services can be created at runtime
-# TODO - pull out broadcast recievers and services from decompiled java code
 printString('\n-- Exported Components --')
 
 # activities
 printString('')
-structured_output['activities'] = []
-getActivities(xmldoc)
+getActivities(xmldoc, conn)
 
 # providers
 printString('')
-structured_output['providers'] = []
-getProviders(xmldoc)
+getProviders(xmldoc, conn)
 
 # Services
 printString('')
-structured_output['services'] = []
-getServices(xmldoc)
+getServices(xmldoc, conn)
 
 # Static receivers
 printString('')
-structured_output['receivers'] = []
-getReceivers(xmldoc)
+getReceivers(xmldoc, conn)
 
-structured_output['runtime_elements'] = {}
-structured_output['runtime_elements']['webviews'] = []
-structured_output['runtime_elements']['js_bridges'] = []
-structured_output['runtime_elements']['services'] = []
-structured_output['runtime_elements']['receivers'] = []
-structured_output['runtime_elements']['broadcasters'] = []
 
 code_search = []
+
 # Now let's hunt through decompiled code :)
 if (java != None):
 	if (debug):
@@ -399,43 +361,37 @@ if (java != None):
 		filedata = f.read()
 		f.close()
 		item_id = fi_name.replace('/', '.')
-		#print item_id
-		'''
-		for item in runtime_elements:
-			if (item in filedata):
-				code_search.append('Detected ' + item + ' in ' + fi_name)
-			structured_output['item'] = fi_name
-		'''
+		
 		if (' WebView' in filedata):
-			#print 'here'
+			print 'here'
 			if (check_all == False):
 				#print packagename
 				#print item_id
 				if (packagename in item_id):
 					code_search.append('[High] Detected WebView in ' + item_id)
-					structured_output['runtime_elements']['webviews'].append(item_id)
+					saveItem(conn, 'webviews', packagename, item_id, "package-match", current_time)
 			else:
 				#print 'check_all false'
 				code_search.append('Detected WebView in ' + item_id)
-				structured_output['runtime_elements']['webviews'].append(item_id)
+				saveItem(conn, 'webviews', packagename, item_id, "", current_time)
 
 		if ('new BroadcastReceiver()' in filedata or 'extends BroadcastReceiver' in filedata):
 			if (check_all == False):
 				if (packagename in item_id):
-					code_search.append('[High] Detected ' + item + ' in ' + item_id)
-					structured_output['runtime_elements']['receivers'].append(item_id)
+					code_search.append('[High] Detected receiver in ' + item_id)
+					saveItem(conn, 'runtime_receivers', packagename, item_id, "package-match", current_time)
 			else:
-				code_search.append('Detected ' + item + ' in ' + item_id)
-				structured_output['runtime_elements']['receivers'].append(item_id)
+				code_search.append('Detected receiver in ' + item_id)
+				saveItem(conn, 'runtime_receivers', packagename, item_id, "", current_time)
 
 		if ('@JavascriptInterface' in filedata):
 			if (check_all == False):
 				if (packagename in item_id):
 					code_search.append('[High] Detected Javascript Bridge in ' + item_id)
-					structured_output['runtime_elements']['js_bridges'].append(item_id)
+					saveItem(conn, 'jsbridges', packagename, item_id, "package-match", current_time)
 			else:
 				code_search.append('Detected Javascript Bridge in ' + item_id)
-				structured_output['runtime_elements']['js_bridges'].append(item_id)
+				saveItem(conn, 'jsbridges', packagename, item_id, "", current_time)
 
 
 
@@ -443,14 +399,15 @@ if (java != None):
 			if (item in filedata and "LocalBroadcastManager" not in filedata):
 				if (packagename in item_id):
 					code_search.append('[High] Detected ' + item + ' in ' + item_id)
-					structured_output['runtime_elements']['broadcasters'].append(item_id)
+					saveItem(conn, 'broadcasters', packagename, item_id, "package-match", current_time)
 			else:
 				if (check_all): # only return low confidence (i.e. probably local broadcast) items if user did not specify only high confidence items
 					code_search.append('Detected ' + item + ' in ' + item_id)
-					structured_output['runtime_elements']['broadcasters'].append(item_id)
+					saveItem(conn, 'broadcaster', packagename, item_id, "", current_time)
 code_search.sort()
 for result in code_search:
 	printString(result)
+
 
 
 ########################
@@ -488,8 +445,5 @@ if (scan_secrets):
 		
 	printString('')
 
+conn.close()
 printString("\nDone")
-
-if (usejson):
-	# actually print the json regardless of the silent mode
-	print json.dumps(structured_output)
